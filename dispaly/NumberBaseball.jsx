fbfdc3009462bdfe80d2634dcdfc3c99a374728b
yu-retel <dbdudals100@naver.com>
2023-08-14 02:31:13 +0900

    feat: 숫자야구게임 중간저장

--- dispaly/NumberBaseball.jsx
+++ dispaly/NumberBaseball.jsx
@@ -1,8 +1,11 @@
 const React = require('react');
 const { useState } = require('react'); 
+
 const NumberBaseball = () => {
   const [value, setValue] = useState('');
   const [result, setResult] = useState('결과');
+  let ballCount = 0;
+  let strikeCount = 0;
 
   const randomNumberGenerator = () => {
     const list = [];
@@ -16,16 +19,46 @@ const NumberBaseball = () => {
     return list;
   }
   
-  const number = randomNumberGenerator();
+  const onChange = (e) => {setValue(e.target.value)};
+
+  const ballCheck = (computer, user) => {
+    computer.map((el, idx) => {
+      if (user.includes(el) && el != user[idx]) {
+        ballCount++;
+      }
+    })
+
+  }
+
+  const strikeCheck = (computer, user) => {
+    computer.map((el, idx) => {
+      if (el === user[idx]) strikeCount ++;
+    })
+  }
+
+  const gameCheck = (e) => {
+    e.preventDefault();
+    const computer = randomNumberGenerator();
+    const user = value.split('').map(Number)
+
+    strikeCheck(computer, user);
+    ballCheck(computer, user);
+
+    if (strikeCount === 3) setResult('정답');
+    else setResult(`${strikeCount} 스트라이크 ${ballCount} 볼`);
+
+    strikeCount = 0;
+    ballCount = 0;
+  }
 
   return (
     <>
     <div>숫자 야구 게임 ⚾️</div>
-    <form>
-      <input />
+    <form onSubmit={gameCheck}>
+      <input type='number' value={value} onChange={onChange}/>
       <button> 입력 </button>
     </form>
-    <div>{number}</div>
+    <div>{result}</div>
     </>
   )
 }
