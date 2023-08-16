2071959b98b7d72da7ed03efb295dd36e6e9eb53
yu-retel <dbdudals100@naver.com>
2023-08-17 02:49:38 +0900

    feat: 숫자야구게임

--- dispaly/NumberBaseball.jsx
+++ dispaly/NumberBaseball.jsx
@@ -1,55 +1,62 @@
-const React = require('react');
-const { useState } = require('react'); 
+import React, { useState } from 'react'; 
+import Try from './Try';
 
-const NumberBaseball = () => {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('결과');
-  let ballCount = 0;
-  let strikeCount = 0;
+const randomNumberGenerator = () => {
+  const list = [];
 
-  const randomNumberGenerator = () => {
-    const list = [];
-
-    while (list.length < 3) {
-      const number = Math.floor(Math.random() * 9) + 1;
-      if (list.includes(number)) continue;
-      list.push(number);
-    }
-
-    return list;
+  while (list.length < 3) {
+    const number = Math.floor(Math.random() * 9) + 1;
+    if (list.includes(number)) continue;
+    list.push(number);
   }
+
+  return list;
+}
+
+const NumberBaseballGame = () => {
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(randomNumberGenerator);
+  //useState의 함수를 넣고 ()하면 호출하기 때문에 ()를 쓰지않으면 lazy init 되어 처음 return 값이 초기값으로 설정되고
+  //불필요한 호출이 일어나지않는다.
+  const [tries, setTries] = useState([]);
+  const [result, setResult] = useState('결과');
   
   const onChange = (e) => {setValue(e.target.value)};
 
-  const ballCheck = (computer, user) => {
-    computer.map((el, idx) => {
-      if (user.includes(el) && el != user[idx]) {
-        ballCount++;
-      }
+  const ballCheck = (answer, user) => {
+    let ball = 0;
+    answer.map((el, idx) => {
+      if (user.includes(el) && el !== user[idx]) ball++;
     })
-
+    return ball;
   }
 
-  const strikeCheck = (computer, user) => {
-    computer.map((el, idx) => {
-      if (el === user[idx]) strikeCount ++;
+  const strikeCheck = (answer, user) => {
+    let strike = 0;
+    answer.map((el, idx) => {
+      if (el === user[idx]) strike++;
     })
+    return strike;
   }
 
-  const [computer, setComputer] = useState(randomNumberGenerator());
-  
   const gameCheck = (e) => {
     e.preventDefault();
-    const user = value.split('').map(Number)
 
-    strikeCheck(computer, user);
-    ballCheck(computer, user);
+    const user = value.split('').map(Number);
+    const strikeCount = strikeCheck(answer, user);
+    const ballCount = ballCheck(answer, user);
 
-    if (strikeCount === 3) setResult('정답');
-    else setResult(`${strikeCount} 스트라이크 ${ballCount} 볼`);
 
-    strikeCount = 0;
-    ballCount = 0;
+    if (strikeCount === 3) {
+      setResult('정답');
+      setTries([]);
+      setAnswer(randomNumberGenerator());
+      setValue('');
+    }
+    else setTries((prevTries) => [...prevTries, { try: value, result: `${strikeCount} 스트라이크 ${ballCount} 볼`}]);
+
+
+    console.log(ballCount,strikeCount, answer)
   }
 
   return (
@@ -59,9 +66,15 @@ const NumberBaseball = () => {
       <input type='number' value={value} onChange={onChange}/>
       <button> 입력 </button>
     </form>
+    <div>시도 : {tries.length} 번</div>
     <div>{result}</div>
+    <ul>{tries.map((el, idx) => {
+      return (
+        <Try key={el + idx} tryInfo={el} />
+      );
+    })}</ul>
     </>
   )
 }
 
-module.exports = NumberBaseball;
+export default NumberBaseballGame;
