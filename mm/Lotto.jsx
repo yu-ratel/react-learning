cd1ef7ad2771e76d8264c4008816a0a6c4be39cd
yu-retel <dbdudals100@naver.com>
2023-09-06 16:08:35 +0900

    feat: React.memo 로 head rerender최적화 및 로또추첨기 완성

--- mm/Lotto.jsx
+++ mm/Lotto.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import numberGenerator from './numberGenerator';
 import NumberColor from './NumberColor';
 
 const Lotto = () => {
   const [jackpotNumber, setJackpotNumber] = useState([]);
   const [bonusNumber, setBonusNumber] = useState(null);
-  const [reset, SetReset] = useState(false);
+  const [reset, setReset] = useState(false);
   
   useEffect(() => {
     const number = numberGenerator();
@@ -23,36 +23,35 @@ const Lotto = () => {
     return (() => {
       clearInterval(interval);
     });
+        // setReset이 한번 재실행할 때 true가 되면서 그 다음부턴 useEffect의 의존성인 reset이 바뀌지않기 때문에 
+    // 위의것들을 다 초기화 한다음 setTimeout으로 reset 초기화
   }, [reset]);
 
   const containerStyle = {
     display: 'flex',
   };
 
-  const onClickReset = () => {
+  const onClickReset = useCallback(() => {
     setJackpotNumber([]);
     setBonusNumber(null);
-    SetReset(true);
-    setTimeout(() => {
-      SetReset(false);
-    })
-    // setReset이 한번 재실행할 때 true가 되면서 그 다음부턴 useEffect의 의존성인 reset이 바뀌지않기 때문에 
-    // 위의것들을 다 초기화 한다음 setTimeout으로 reset 초기화
-  }
+    setReset((prevReset) => !prevReset);
+  }, []);
 
+
+  const MemoizedHeader = React.memo(() => (
+    <>
+      <div>당첨 번호</div>
+      <div style={containerStyle}>{jackpotNumber.map((el) => <NumberColor key={el} ballNumber={el} />)}</div>
+      <div>보너스 번호</div>
+    </>
+  ));
 
   return (
     <>
-    <div>당첨 번호</div>
-    <div style={containerStyle}>{jackpotNumber.map((el) => {
-      return (
-        <div id={'numberBall'} className={NumberColor(el)}>{el}</div>
-      );
-    })}</div>
-    <div>보너스 번호</div>
+    <MemoizedHeader />
     {bonusNumber !== null && (
       <>
-      <div id={'numberBall'} className={NumberColor(bonusNumber)}>{bonusNumber}</div>
+      <div> <NumberColor key={bonusNumber} ballNumber={bonusNumber} /></div>
       <button onClick={onClickReset}>재시작</button>
       </>
     )}
@@ -61,3 +60,9 @@ const Lotto = () => {
 }
 
 export default Lotto;
+
+
+//useMemo = 결과값을 기억,  useCallback = 함수를 기억
+// 두번째 인자값에서 기본값을 잘못설정해주면 기억을 잘해서 새로 값이 되어도 원본이 바뀌질 않는다.
+// 그래서 두번째 인자값에는 변경될 때 변경되야하는 인자를 넣어주는게 중요하다.
+
--- mm/NumberColor.js
+++ mm/NumberColor.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { memo } from 'react';
 
-const NumberColor = (ballNumber) => {
-  if (ballNumber < 10) return 'yellow';
-  if (ballNumber < 20) return 'blue';
-  if (ballNumber < 30) return 'red';
-  if (ballNumber < 40) return 'gray';
-  return 'green';
-}
+const NumberColor = memo(({ ballNumber }) => {
+  let background;
+  if (ballNumber < 10) background = 'yellow';
+  else if (ballNumber < 30) background = 'red';
+  else if (ballNumber < 20) background = 'blue';
+  else if (ballNumber < 40) background = 'gray';
+  else background = 'green';
+
+  return (
+    <div className='numberBall' style={{ background }}>{ballNumber}</div>
+  )
+});
 
 export default NumberColor;
